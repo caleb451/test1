36330a8a2f00d1caafb33b7413a38e476f4c5e2d
caleb451 <43527078+caleb451@users.noreply.github.com>
2022-12-15 18:25:33 -0600

    reupload

---
+++ app.py
@@ -0,0 +1,10 @@
+# Import necessary packages and functions
+from flask import Flask, render_template
+
+# Create our web application
+app = Flask(__name__)
+
+# Create our routes
+@app.route('/')
+def home():
+	return render_template('index.html')

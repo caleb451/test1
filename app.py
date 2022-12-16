b352f45babcadd4d061431acd6efb70eaca50bc0
caleb451 <43527078+caleb451@users.noreply.github.com>
2022-12-15 21:28:45 -0600

    updated a lot

--- app.py
+++ app.py
@@ -1,18 +1,12 @@
 # Import necessary packages and functions
 from flask import Flask, render_template
+import random
 
 # Create our web application
 app = Flask(__name__)
 
 # Create our routes
-@app.route('/git_update', methods=['POST'])
-def git_update():
-	repo = git.Repo('./portfolio_final')
-	origin = repo.remotes.origin
-	repo.create_head('main', origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
-	origin.pull()
-	return '', 200
-
 @app.route('/')
 def home():
-	return render_template('index.html')
+    return render_template('index.html')
+
